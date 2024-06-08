f1c86fbd9f0361bc69f47a76c69dfd2e83076fac
Panthomema <71283471+Panthomema@users.noreply.github.com>
2024-06-08 02:17:47 +0200

    Add dynamic web apps (finished)

--- dynamic-web-apps/index.php
+++ dynamic-web-apps/index.php
@@ -1,6 +1,33 @@
 <?php
 
 require 'functions.php';
+//require 'router.php';
 
-require 'router.php';
+require 'Database.php';
 
+$config = require('config.php');
+
+$db = new Database($config['database']);
+
+$id = $_GET['id'];
+
+/* Both are okay
+$query = "select * from posts where id = ?";
+$post = $db->query($query, [$id])->fetch();
+*/
+
+// This accepts more parameters
+$query = "select * from posts where id = :id";
+$post = $db->query($query, [':id' => $id])->fetch();
+
+dd($post);
+
+/*
+foreach ($posts as $post) {
+  echo '<ul>';
+  foreach ($post as $key => $value) {
+    echo "<li>{$key}: $value</li>";
+  }
+  echo '</ul>';
+}
+*/
